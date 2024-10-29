6315ce0ad05d0d470de71514c69ade15bc0a39d2
Трофимов Алексей <a.trofimov@domain.local>
2024-10-29 14:03:18 +0300

    chore(database): drizzle sh*t

--- apps/express-server/app.ts
+++ apps/express-server/app.ts
@@ -1,16 +1,23 @@
 import db from "@repo/database";
 import bodyParser from "body-parser";
 import express from "express";
-// import { usersTable } from "@repo/database/schema"
+import { usersTable } from "@repo/database/schema"
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+
+const users = await db.select().from(usersTable)
+console.log(users)
 
 app.post("/api/person", async (req, res) => {
 	const { first_name, last_name } = req.body;
 	console.log(db, first_name, last_name);
+
+	const users = await db.select().from(usersTable)
+	console.log(users)
 	// await db
 	// 	.insert(usersTable)
 	// 	.values({ first_name, last_name, created_at: Date.now() });
--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 
-const db = drizzle(process.env.DB_FILE_NAME!);
+const db = drizzle({
+	connection: {
+		url: process.env.DB_FILE_NAME!,
+		authToken: "",
+	},
+});
 
 export default db;
