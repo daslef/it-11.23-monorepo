40e1e089a367ae365cfa6aabdc604bd9f85778bd
Трофимов Алексей <a.trofimov@domain.local>
2024-10-29 13:17:05 +0300

    fix(global): integration fix

--- apps/express-server/app.ts
+++ apps/express-server/app.ts
@@ -1,6 +1,6 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import db from "@repo/database"
+import db from "@repo/database";
+import bodyParser from "body-parser";
+import express from "express";
 // import { usersTable } from "@repo/database/schema"
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/api/person", async (req, res) => {
 	const { first_name, last_name } = req.body;
-	console.log(db, first_name, last_name)
+	console.log(db, first_name, last_name);
 	// await db
 	// 	.insert(usersTable)
 	// 	.values({ first_name, last_name, created_at: Date.now() });
