ed780426445b62c73c5516297aa45fec02990adb
Трофимов Алексей <a.trofimov@domain.local>
2024-11-07 17:02:27 +0300

    chore(news): news app styling

--- apps/news/app.ts
+++ apps/news/app.ts
@@ -6,10 +6,21 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.set("view engine", "ejs");
 
 app.get("/:quantity/news/for/:category", async (req, res) => {
 	const { quantity, category } = req.params;
-	res.send({ quantity, category });
+	const news = [
+		{
+			title: "Новость важная",
+			category: "Авто",
+		},
+		{
+			title: "Новость актуальная",
+			category: "Здоровье",
+		},
+	];
+	res.render("index", { news, quantity, category });
 });
 
 app.listen(3000);
