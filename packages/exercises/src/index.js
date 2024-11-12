d77771d39f72aae50d687f48816cf452044a8eac
Трофимов Алексей <a.trofimov@domain.local>
2024-11-12 13:11:11 +0300

    style(exercises): fix styling

--- packages/exercises/src/index.js
+++ packages/exercises/src/index.js
@@ -1,10 +1,10 @@
 function parseArrayEntries(entries) {
-  /**
-   * @param [['id', 7], ['hash', 'hsk471']]
-   * @returns { id: 7, hash: 'hsk471'}
-   */
+	/**
+	 * @param [['id', 7], ['hash', 'hsk471']]
+	 * @returns { id: 7, hash: 'hsk471'}
+	 */
 
-  return Object.fromEntries(entries);
+	return Object.fromEntries(entries);
 }
 
 export { parseArrayEntries };
--- packages/exercises/tests/parseEntries.test.js
+++ packages/exercises/tests/parseEntries.test.js
@@ -2,10 +2,10 @@ import test from "node:test";
 import { parseArrayEntries } from "../src/index.js";
 
 test("parse correct pair of entries", (t) => {
-  const params = [
-    ["id", 7],
-    ["hash", "hsk471"],
-  ];
-  const expected = { id: 7, hash: "hsk471" };
-  t.assert.deepStrictEqual(parseArrayEntries(params), expected);
+	const params = [
+		["id", 7],
+		["hash", "hsk471"],
+	];
+	const expected = { id: 7, hash: "hsk471" };
+	t.assert.deepStrictEqual(parseArrayEntries(params), expected);
 });
